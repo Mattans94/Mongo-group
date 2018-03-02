cae03b23c94bac739d0fb3e168b3839ae474f175
Marcus Thelin <marcusthelin97@gmail.com>
2018-03-02 13:18:34 +0100

    Added code for SPA

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json()) // needed to post json
 app.use(express.static('www'));
+
+// Serve index.html if req has no file extension.
+// (to work with SPA)
+app.get(/^[^\.]*$/, (req, res) => {
+  res.sendFile(__dirname + '/www/index.html');
+});
+
+
 app.listen(3000,()=>{
   console.log("Listening on port 3000!");
 });
