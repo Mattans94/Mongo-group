7c29a82248247afb49596cdec1cf94969ee6cec9
Nana Mochihara <nanahaan@gmail.com>
2018-03-03 21:41:28 +0100

    Import beans and powders

--- importer.js
+++ importer.js
@@ -1,8 +1,10 @@
-const kaffeJson = require('./kaffe.json');
+const beansJson = require('./beans.json');
+const powderJson = require('./powders.json');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
 mongoose.connect('mongodb://localhost/coffeeDB');
 const db = mongoose.connection;
 db.on('error', (e) => {
@@ -11,36 +13,34 @@ db.on('error', (e) => {
 db.once('open', () => {
   console.info('db connected');
 });
+
 const Bean = require('./classes/Bean.class');
 const Powder = require('./classes/Powder.class');
-const Capsule = require('./classes/Capsule.class');
-const Tool = require('./classes/Tool.class');
 const beanModel = new Bean(app).myModel;
 const powderModel = new Powder(app).myModel;
-const capsuleModel = new Capsule(app).myModel;
-const toolModel = new Tool(app).myModel;
 
 // Empty collentions
-beanModel.remove({}, () => {
-  powderModel.remove({}, () => {
-    capsuleModel.remove({}, () => {
-      toolModel.remove({}, async () => {
-        await start();
-        process.exit();
-      });
-    });
-  });
+beanModel.remove({}, async () => {
+  await saveBean();
 });
 
-const start = () => {
+powderModel.remove({}, async () => {
+  await savePowder();
+  // process.exit();
+});
 
-  const bean = new beanModel;
-  bean.save();
-  const powder = new powderModel;
-  powder.save();
-  const capsule = new capsuleModel;
-  capsule.save();
-  const tool = new toolModel;
-  tool.save();
+const saveBean = () => {
+  beansJson.forEach((item) => {
+    const bean = new beanModel(item);
+    bean.save();
+    console.log("beans saved");
+  });
+}
 
+const savePowder = () => {
+  powderJson.forEach((item) => {
+    const powder = new powderModel(item);
+    powder.save();
+    console.log("powder saved");
+  });
 }
