437022c181daf10b1429130d5508f6d426905fd6
huisusu <yudasuhui@hotmail.com>
2018-03-03 19:57:07 +0100

    checkout

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -2,8 +2,8 @@ class App extends Base {
     constructor() {
         super();
         this.rest = new REST();
-        this.cart = new Cart(this.rest);
         this.profile=new Profile();
+        this.cart = new Cart(this.rest, this.profile);
         this.changePage();
         this.clickEvents();
     }
@@ -16,6 +16,19 @@ class App extends Base {
             this.cart.renderShoppingList();
             this.cart.renderTotalPriceWithVAT();
         }
+        if (url == '/register') {
+            $('main').empty();
+            this.profile.render('main', 'Register');
+        }
+        if (url == '/checkout') {
+            $('main').empty();
+            this.cart.render('main', 'CheckOut');
+            this.cart.renderTotalPriceWithVAT();
+            this.profile.render('.stepBox','Address');
+        }
+    
+
+
     }
     clickEvents() {
         let that=this;
--- www/js/classes/cart.class.js
+++ www/js/classes/cart.class.js
@@ -1,7 +1,9 @@
 class Cart extends Base {
-    constructor() {
+    constructor(rest, profile) {
         super();
-        
+        this.rest=rest;
+        this.profile=profile;
+        this.clickEvents();
     }
 
     renderShoppingList(){
@@ -13,4 +15,35 @@ class Cart extends Base {
         this.render('#order-summary', 'OrderSummary');
 
     }
+    clickEvents() {
+        let that=this;
+        $(document).on("click", '.address-btn', function () {
+            $(".checkOut-btns").removeClass("active");
+            $(".address-btn").addClass("active");
+            $('.stepBox').empty();
+            that.profile.render('.stepBox','Address');
+        });
+        $(document).on("click", '.delivery-btn', function () {
+            $(".checkOut-btns").removeClass("active");
+            $(".delivery-btn").addClass("active");
+            $('.stepBox').empty();
+            that.render('.stepBox','Delivery');
+        });
+        $(document).on("click", '.payment-btn', function () {
+            $(".checkOut-btns").removeClass("active");
+            $(".payment-btn").addClass("active");
+            $('.stepBox').empty();
+            that.render('.stepBox','Payment');
+        });
+        $(document).on("click", '.review-btn', function () {
+            $(".checkOut-btns").removeClass("active");
+            $(".review-btn").addClass("active");
+            $('.stepBox').empty();
+            that.render('.stepBox','Review');
+            that.renderShoppingList();
+        });
+
+
+    }
+   
 }
