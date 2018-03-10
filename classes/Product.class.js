26d4850a935dcfe3df0e087dac0c6b2ccce0cfbe
Nana Mochihara <nanahaan@gmail.com>
2018-03-10 14:21:05 +0100

    Add quantity in stock

--- classes/Product.class.js
+++ classes/Product.class.js
@@ -13,6 +13,7 @@ module.exports = class Product extends ModelAndRoutes {
       flavor: String,
       countryOfOrigin: String,
       type: String,
+      stock: Number,
       connectType: Number,
       tools: [{
         type: Schema.Types.ObjectId,
