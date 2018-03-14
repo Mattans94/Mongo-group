c128ccd88d15a513425f79c093f3a39ff289d334
Unknown <omarkingen10@gmail.com>
2018-03-14 11:17:54 +0100

    Plus and minus button fixed

--- www/js/classes/info.class.js
+++ www/js/classes/info.class.js
@@ -4,4 +4,17 @@ class Info extends REST {
 		this.app = app;
 	}
 
+	click(e) {
+		if ($(e.target).is('#plus-btn') || $(e.target).parent().is('#plus-btn')){
+			console.log('plus');
+			$("#quantity").val(parseInt($('#quantity').val(), 10)+1);
+		}
+		else if ($(e.target).is('#minus-btn') || $(e.target).parent().is('#minus-btn')){
+			console.log('minus');
+			$("#quantity").val(parseInt($('#quantity').val(), 10)-1)
+		}
+	}
 }
+
+//<button id="plus-btn">+</button>
+//<button id="minus-btn">-</button>
