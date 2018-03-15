26f5cc6d461c03c763d3dfceeb3e6b2e40ea9da3
Emil <emil_emil94@hotmail.com>
2018-03-15 09:33:01 +0100

    removed user

--- classes/user.class.js
+++
@@ -1,9 +0,0 @@
-// const Schema = require('mongoose').Schema;
-// const ModelAndRoutes = require('./model-and-routes.class');
-//
-// module.exports = class User extends ModelAndRoutes {
-//   static get schema (){
-//     path: String,
-//     role: [String]
-//   }
-// }
