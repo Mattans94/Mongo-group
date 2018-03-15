babea2a09fc225f690cc861cd62f60c190c4f568
Unknown <omarkingen10@gmail.com>
2018-03-15 12:00:00 +0100

    Button changes

--- www/js/classes/info.class.js
+++ www/js/classes/info.class.js
@@ -6,12 +6,11 @@ class Info extends REST {
 
 	click(e) {
 		if ($(e.target).is('#plus-btn') || $(e.target).parent().is('#plus-btn')){
-			console.log('plus');
-			$("#quantity").val(parseInt($('#quantity').val(), 10)+1);
+			$("#quantity").val(parseInt($('#quantity').val())+1);
+			val++;
 		}
 		else if ($(e.target).is('#minus-btn') || $(e.target).parent().is('#minus-btn')){
-			console.log('minus');
-			$("#quantity").val(parseInt($('#quantity').val(), 10)-1)
+			$("#quantity").val(parseInt($('#quantity').val())-1)
 		}
 	}
 }
