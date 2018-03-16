e1a4aaa139362d2317376cb673dd67c7c691a124
Nana Mochihara <nanahaan@gmail.com>
2018-03-16 15:25:51 +0100

    Implement makeOrderList in user page

--- www/js/classes/user-page.class.js
+++ www/js/classes/user-page.class.js
@@ -3,4 +3,33 @@ class UserPage extends Base {
     super();
     this.app = app;
   }
+
+  makeOrderList() {
+    const orderList = [];
+    // temporary orders (this.app.orders is all orders in our site)
+    const targets = this.app.orders;
+
+    targets.forEach(target => {
+      orderList.unshift(`
+      <tr>
+        <th id="orderNumber">${target.orderNumber}</th>
+        <td>${moment(target.orderTime).format('YYYY-MM-DD')}</td>
+        <td>3</td>
+        <td>${target.total}</td>
+        <td>${target.status}</td>
+      </tr>
+      `);
+    });
+    return orderList.join('');
+  }
+
+  // We use this after user and order connected
+  renderList(user) {
+    const filterTargets = this.app.orders.filter(order => {
+      return order.user === user;
+    });
+    $('#order-list').empty();
+    $('#order-list').append(this.makeOrderList(filterTargets));
+  }
+
 }
