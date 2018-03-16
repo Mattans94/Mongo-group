6a73a59889c80f196dd217cdd4bbe0e845968f10
Petter <petter.f.andersson@gmail.com>
2018-03-16 18:08:12 +0100

    Add generic preview message

--- www/js/classes/app.class.js
+++ www/js/classes/app.class.js
@@ -81,5 +81,14 @@ class App extends REST {
           pass: account.pass
         }
       });
+
+      let message = {
+        from: 'sender@server.com',
+        to: 'receiver@sender.com',
+        subject: 'Message title',
+        text: 'Plaintext version of the message',
+        html: '<p>HTML version of the message</p>'
+      };
+
     }
 }
