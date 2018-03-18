7f2cf74991a81d99127cb18a757d38f7f089cc06
Aiko M <minmilera@gmail.com>
2018-03-18 09:30:41 +0100

    Add dropdown button in navbar

--- www/js/classes/navbar.class.js
+++ www/js/classes/navbar.class.js
@@ -15,4 +15,60 @@ class Navbar extends REST{
       item.active = url == item.url;
     }
   }
+
+  
+
+
+  
+  templateLogginButton() {
+    <button id="loginModalToggle" class="btn">Logga in<i class="fas fa-sign-in-alt ml-1 fa-lg"></i></button>
+  }
+  
+
+  templateNormalUser() {
+    
+    <div class="dropdown">
+    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="far fa-user"></i>
+        ${this.app.currentUser}
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#"></a>
+      <a class="dropdown-item" href="/mina_sidor">Mina sidor</a>
+      <a class="dropdown-item" href="/">Logga ut<i class="fas fa-sign-out-alt ml-1 fa-lg"></i></a>
+    </div>
+  </div>
+
+  }
+
+ 
+
+
+    // change the text of the login button (user/admin)
+    // change the url (user: mina_sidor, admin: admin )
+    // dropdown button (mina sidor, log out)
+
+
+
+    // Change button if user log in
+
+
+    // changeButton() {
+    //   // let that = this;
+    //   // let user = that.app.currentUser
+    //   if(nej) { return }
+
+    //   else if (normal) {
+    //   //   this.app.
+    //   }
+    //   else if(admin) {
+
+    //   }
+
+    // }
+
+
+
+
+
 }
