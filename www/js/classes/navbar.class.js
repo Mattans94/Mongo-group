69ca61aee790a824f1371509d855aa20ddbd685c
Aiko M <minmilera@gmail.com>
2018-03-19 09:28:45 +0100

    Uppdate function i navbar

--- www/js/classes/navbar.class.js
+++ www/js/classes/navbar.class.js
@@ -18,32 +18,32 @@ class Navbar extends REST {
     }
   }
 
-  // LogginBtnForNormalUser() {
-  //   let that = this;
-  //   $(document).on('click', '.pop', () => {
+  LogginBtnForNormalUser() {
+    let that = this;
+    $(document).on('click', '.pop', () => {
 
-  //     $('.modal').modal('hide');   // Close the modal after logg in
-  //     location.href = "/mina_sidor";  // Link to mina_sidor
+      $('.modal').modal('hide');   // Close the modal after logg in
+      location.href = "/mina_sidor";  // Link to mina_sidor
 
-  //     console.log('User:', that.app.currentUser)
+      console.log('User:', that.app.currentUser)
 
-  //     $('#loginModalToggle').replaceWith(function () {
-  //       $('#loginModalToggle').replaceWith(`<div class="dropdown">
-  //     <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  //       <i class="far fa-user"></i>
-  //       ${that.app.currentUser} 
-  //     </button>
-  //     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-  //       <a class="dropdown-item" href="#"></a>
-  //       <a class="dropdown-item" href="/mina_sidor" id="myPage">Mina sidor</a>
-  //       <a class="dropdown-item" href="/">Logga ut
-  //         <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
-  //       </a>
-  //     </div>
-  //   </div>`)
-  //     });
-  //   })
-  // }
+      $('#loginModalToggle').replaceWith(function () {
+        $('#loginModalToggle').replaceWith(`<div class="dropdown">
+      <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="far fa-user"></i>
+        ${that.app.currentUser} 
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#"></a>
+        <a class="dropdown-item" href="/mina_sidor" id="myPage">Mina sidor</a>
+        <a class="dropdown-item" href="/">Logga ut
+          <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
+        </a>
+      </div>
+    </div>`)
+      });
+    })
+  }
 
 
   LogginBtnForAdmin() {
@@ -57,7 +57,7 @@ class Navbar extends REST {
 
       $('#loginModalToggle').replaceWith(function () {
         $('#loginModalToggle').replaceWith(`<div class="dropdown">
-      <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="far fa-user"></i>
         ${that.app.currentUser} 
       </button>
