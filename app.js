ed470384cb0a7458ece7a3d72e76f3c0133bc175
Petter <petter.f.andersson@gmail.com>
2018-03-19 11:47:53 +0100

    Commented out sendMail method to not send on every file save

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(express.static('www'));
 app.use(flexjson);
 app.use(cookieParser()); // needed to read and set cookies
 app.use(session);
-// app.use(nodemailer);
 
 
 const Product = require('./classes/Product.class');
@@ -56,12 +55,12 @@ let mailOptions = {
   html: '<h1>Tack för att du valde att köpa ditt kaffe från CoffeeDB!</h1><p><strong>Du har beställt:</strong></p><ul><li>Test1</li><li>Test2</li><li>Test3</li></ul><p style="margin-bottom: 30px;"><strong>Total kostnad:</strong> XX kr</p><p style="margin-bottom: 30px;">Din beräknade leveranstid är 2-3 arbetsdagar.</p><hr><p><em>Har du problem, frågor eller funderingar? Tveka inte att höra av dig till oss på 040 - 12 33 21 eller maila till info@coffeeDB.com.</em></p>'
 };
 
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-      return console.log(error);
-  }
-  console.log('Message sent: %s', info.messageId);
-});
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//       return console.log(error);
+//   }
+//   console.log('Message sent: %s', info.messageId);
+// });
 
 
 // Serve index.html if req has no file extension.
