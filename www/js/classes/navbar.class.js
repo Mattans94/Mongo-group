629859432d75815329775055a2f412fa783f28f2
Aiko M <minmilera@gmail.com>
2018-03-19 12:03:56 +0100

    /login-button: Auto stash before merge of "feature/login-button" and "develop"

--- www/js/classes/navbar.class.js
+++ www/js/classes/navbar.class.js
@@ -18,31 +18,38 @@ class Navbar extends REST {
     }
   }
 
+  changeLoginBtn() {
+    if(app.checkIfLogin()) {
+      console.log(app.checkIfLogin())
+    }
+  }
+
+
   LogginBtnForNormalUser() {
     let that = this;
     $(document).on('click', '.pop', () => {
-
+      this.changeLoginBtn();
       $('.modal').modal('hide');   // Close the modal after logg in
-      location.href = "/mina_sidor";  // Link to mina_sidor
+      // location.href = "/mina_sidor";  // Link to mina_sidor
 
       console.log('User:', that.app.currentUser)
 
-      $('#loginModalToggle').replaceWith(function () {
-        $('#loginModalToggle').replaceWith(`<div class="dropdown">
-      <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="far fa-user"></i>
-        ${that.app.currentUser} 
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#"></a>
-        <a class="dropdown-item" href="/mina_sidor" id="myPage">Mina sidor</a>
-        <a class="dropdown-item" href="/">Logga ut
-          <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
-        </a>
-      </div>
-    </div>`)
-      });
-    })
+    //   $('#loginModalToggle').replaceWith(function () {
+    //     $('#loginModalToggle').replaceWith(`<div class="dropdown">
+    //   <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //     <i class="far fa-user"></i>
+    //     ${that.app.currentUser} 
+    //   </button>
+    //   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //     <a class="dropdown-item" href="#"></a>
+    //     <a class="dropdown-item" href="/mina_sidor" id="myPage">Mina sidor</a>
+    //     <a class="dropdown-item" href="/">Logga ut
+    //       <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
+    //     </a>
+    //   </div>
+    // </div>`)
+    //   });
+    });
   }
 
 
@@ -55,24 +62,27 @@ class Navbar extends REST {
       
       console.log('User:', that.app.currentUser)
 
-      $('#loginModalToggle').replaceWith(function () {
-        $('#loginModalToggle').replaceWith(`<div class="dropdown">
-      <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="far fa-user"></i>
-        ${that.app.currentUser} 
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#"></a>
-        <a class="dropdown-item" href="/admin" id="myPage">Admin sidor</a>
-        <a class="dropdown-item" href="/">Logga ut
-          <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
-        </a>
-      </div>
-    </div>`)
-      });
-    })
+    //   $('#loginModalToggle').replaceWith(function () {
+    //     $('#loginModalToggle').replaceWith(`<div class="dropdown">
+    //   <button class="btn dropdown-toggle mr-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //     <i class="far fa-user"></i>
+    //     ${that.app.currentUser} 
+    //   </button>
+    //   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //     <a class="dropdown-item" href="#"></a>
+    //     <a class="dropdown-item" href="/admin" id="myPage">Admin sidor</a>
+    //     <a class="dropdown-item" href="/">Logga ut
+    //       <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
+    //     </a>
+    //   </div>
+    // </div>`)
+    //   });
+    });
 
   }
+
+
+  
 
 
 
