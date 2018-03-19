8c6d60f5d0a8102f6d3dd38bcaa0297ee8d835cb
Aiko M <minmilera@gmail.com>
2018-03-19 09:30:11 +0100

    Update button i loggin-btn-admin.js

--- www/templates/navbar-class/loggin-btn-admin.js
+++ www/templates/navbar-class/loggin-btn-admin.js
@@ -1,17 +1,17 @@
 Object.assign(Navbar.prototype, { template4(){ return `
 
 <div class="dropdown">
-	<button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		<i class="far fa-user"></i>
-		Admin
-	</button>
-	<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-		<a class="dropdown-item" href="#"></a>
-		<a class="dropdown-item" href="/admin">Admin sidor</a>
-		<a class="dropdown-item" href="/">Logga ut
-			<i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
-		</a>
-	</div>
-</div>
+      <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="far fa-user"></i>
+        ${that.app.currentUser} 
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#"></a>
+        <a class="dropdown-item" href="/admin" id="myPage">Admin sidor</a>
+        <a class="dropdown-item" href="/">Logga ut
+          <i class="fas fa-sign-out-alt ml-1 fa-lg"></i>
+        </a>
+      </div>
+    </div>
 
 `;}});
