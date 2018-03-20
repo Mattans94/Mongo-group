901e4761760c1154125e8255eaf2d52429157da4
Petter <petter.f.andersson@gmail.com>
2018-03-20 15:45:07 +0100

    Delete redundant code from app.js

--- app.js
+++ app.js
@@ -33,30 +33,6 @@ const orderDetails = new OrderDetails(app);
 const sendmail = new Sendmail(app);
 
 
-// Move to appropriate class
-// Enter real values (sender, reciever, bought objects etc.)
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'coffedb@gmail.com',
-//     pass: 'adminpassword'
-//   }
-// });
-
-// let mailOptions = {
-//   from: 'coffedb@gmail.com',
-//   to: 'coffedb@gmail.com',
-//   subject: 'Bekräftelse på din beställning',
-//   html: '<h1>Tack för att du valde att köpa ditt kaffe från CoffeeDB!</h1><p><strong>Du har beställt:</strong></p><ul><li>Test1</li><li>Test2</li><li>Test3</li></ul><p style="margin-bottom: 30px;"><strong>Total kostnad:</strong> XX kr</p><p style="margin-bottom: 30px;">Din beräknade leveranstid är 2-3 arbetsdagar.</p><hr><p><em>Har du problem, frågor eller funderingar? Tveka inte att höra av dig till oss på 040 - 12 33 21 eller maila till info@coffeeDB.com.</em></p>'
-// };
-
-// transporter.sendMail(mailOptions, (error, info) => {
-//   if (error) {
-//       return console.log(error);
-//   }
-//   console.log('Message sent: %s', info.messageId);
-// });
-
 app.get('/getLogin', (req, res) => {
   //const Session = mongoose.model('Session', session.schema);
   let query = session.schema.findOne({ _id: req.cookies.session });
